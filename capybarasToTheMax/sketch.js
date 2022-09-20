4609042d4b99916d7c756049be64acbb02007d40
AngelaZhao06 <114016257+AngelaZhao06@users.noreply.github.com>
2022-09-20 12:15:58 -0600

    Capybara
    Capybara

---
+++ capybarasToTheMax/sketch.js
@@ -0,0 +1,22 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let img;
+
+function preload() {
+  img = loadImage("capybara0.jpg'");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  image(img, 0, 0);
+}
+
+function draw() {
+  background(220);
+}
+
