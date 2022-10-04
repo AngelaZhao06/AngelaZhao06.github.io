0214513818f4b2565a3e146b52910e71d5e0143d
AngelaZhao06 <114016257+AngelaZhao06@users.noreply.github.com>
2022-10-04 12:17:11 -0600

    capy

--- capybarasToTheMax/sketch.js
+++ capybarasToTheMax/sketch.js
@@ -5,36 +5,38 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let img;
-let x, y, d;
-let i, a;
+const circles = [{
+  x:200, y:200, r:200 
+}];
 
 function preload() {
-  img = loadImage("capybara.jfif");
+  //img = loadImage("capybara.jfif");
   createCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
-  x = windowWidth/2;
-  y = windowHeight/2;
-  d = windowHeight*0.75;
-
 }
 
 function draw() {
   background("white");
-  splitCircle();
+  drawCircle(circles[0]);
+  // splitCircle();
   // image(img, width/2, height/2); 
 }
 
+function drawCircle(circle){
+  ellipse(circle.x, circle.y, circle.r * 2, circle.r * 2);
+}
+
 function splitCircle(){
-  for (i = 1; i < 2; i ++){
-    circle(x)
+  for (let i = 0; i < circles.length; i ++){
+    var splitcircle = circles[i];
+    if (mouseX)
   }
   
 }
 
-function mouseClicked(){
-}
+// function mouseClicked(){
+// }
